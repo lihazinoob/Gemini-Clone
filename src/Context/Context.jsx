81ccf3719aa71ca0adbb6b0d09d2c5f5114fa5a6
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-11-03 23:00:41 +0600

    API is integrated and Shown on the UI successfully

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -9,14 +9,24 @@ const GlobalContextProvider = (props) => {
   // Setting some state Variables 
 
   const [input, setInput] = useState(""); //For taking the input of user
-
-
-
+  const [resultData, setResultData] = useState(""); //For storing the result of the API
+  const [loading, setLoading] = useState(false); //For showing the loading screen
+  const [showResult, setShowResult] = useState(false); //For showing the result screen
+  const [recentPrompt,setRecentPrompt] = useState(""); //For storing the recent prompt
+  
   {/* Ami ekhn ekta function banabo jeita geini.js
     theke run function ke call korbe */}
     const OnSent = async(userPrompt) => 
     {
-      await run(input);
+      setResultData("");
+      setLoading(true);
+      setShowResult(true);
+      setRecentPrompt(input);
+      const result = await run(input);
+      setResultData(result);
+      setLoading(false);
+      setInput("");
+
     };
 
 
@@ -24,7 +34,12 @@ const GlobalContextProvider = (props) => {
     const contextValue ={
       input,
       setInput,
-      OnSent
+      OnSent,
+      showResult,
+      recentPrompt,
+      setRecentPrompt,
+      resultData,
+      setResultData
     }
 
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,30 +1,24 @@
-import React, { useContext } from 'react'
-import './Main.css'
-import { assets } from '../../assets/assets'
-import { GlobalContext } from '../../Context/Context'
+import React, { useContext } from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { GlobalContext } from "../../Context/Context";
 const Main = () => {
-
   // Importing the context from Context.js in here
 
-  const {input,setInput,OnSent}  = useContext(GlobalContext);
+  const { input, setInput, OnSent, showResult,recentPrompt,setRecentPrompt , resultData , setResultData} = useContext(GlobalContext);
 
   // Function to change the state of this component when there is some change in the input field
 
-  const changeInput = (event) => 
-  {
+  const changeInput = (event) => {
     // setInput(event.target.value);
-    
-    
-      setInput(event.target.value);
-      console.log(event.target.value);
-    
-    
-  }
 
-  const clickSend = () =>
-  {
+    setInput(event.target.value);
+    // console.log(input);
+  };
+
+  const clickSend = () => {
     OnSent();
-  }
+  };
 
   return (
     <div className="main">
@@ -33,35 +27,57 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p><span>Hello,DEV</span></p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places to see an upcoming road</p>
-            <img src={assets.compass_icon} alt="" />
-          </div>
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello,DEV</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see an upcoming road</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
 
-          <div className="card">
-            <p>Briefly summarize this concept: Urban Planning</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
+              <div className="card">
+                <p>Briefly summarize this concept: Urban Planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
 
-          <div className="card">
-            <p>Brainstorm Team Bonding activities for our work:</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
+              <div className="card">
+                <p>Brainstorm Team Bonding activities for our work:</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
 
-          <div className="card">
-            <p>Improve the readibility of the following code: </p>
-            <img src={assets.code_icon} alt="" />
+              <div className="card">
+                <p>Improve the readibility of the following code: </p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-data">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              <p>{resultData}</p>
+            </div>
           </div>
-        </div>
+        )}
 
         <div className="main-bottom">
           <div className="search-box">
-            <input value = {input} onChange = {changeInput} type="text" placeholder='Enter a Prompt Here' />
+            <input
+              value={input}
+              onChange={changeInput}
+              type="text"
+              placeholder="Enter a Prompt Here"
+            />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
@@ -71,12 +87,10 @@ const Main = () => {
           <p className="bottom-info">
             Gemini may display inaccurate info, please cross check.
           </p>
-
         </div>
-
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Main
+export default Main;
